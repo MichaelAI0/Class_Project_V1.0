ada06ef721b52d459302abde6d7e1ca4a438556a
Michael Harris <alphasni@mail.com>
2022-05-09 20:57:23 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ApiService } from './published/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bibber'
+
+  constructor(
+    private apiService: ApiService
+    ){}
+
+  ngOnInit(){}
+
+
+}
